eb017ecd608553e383180c15cdbd2133ea067553
Stuart Ingram <ingrams2@upmc.com>
2016-10-06 12:27:46 -0400

    Springboot & camel framework initialized

---
+++ src/main/java/com/srai/SpringBootCamelExampleApplication.java
@@ -0,0 +1,12 @@
+package com.srai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCamelExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCamelExampleApplication.class, args);
+	}
+}
