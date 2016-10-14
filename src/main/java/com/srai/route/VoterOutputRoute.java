e644792876b4098f26d0434a7c7cacd6b33edde6
Stuart Ingram <ingrams2@upmc.com>
2016-10-13 21:44:19 -0400

    Dockerized app.  Added outbound sink to REDIS.

--- src/main/java/com/srai/route/VoterOutputRoute.java
+++ src/main/java/com/srai/route/VoterOutputRoute.java
@@ -1,10 +1,20 @@
 package com.srai.route;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.redis.RedisConstants;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class VoterOutputRoute extends RouteBuilder {
+
+  @Bean
+  public RedisSerializer stringSerializer() {
+    return new StringRedisSerializer();
+  }
 
   @Override
   public void configure() throws Exception {
@@ -15,10 +25,13 @@ public class VoterOutputRoute extends RouteBuilder {
     // Output registered voters to websocket
     .to("websocket://localhost:9292/echo?sendToAll=true")
 
-    // Aggregate votes by candidate
+    // Aggregate votes by candidate to REDIS
     .pipeline()
     .aggregate(simple("${body.getCandidate}"), new VoteCountAggregationStrategy()).completionTimeout(1000L)
-    .to("stream:out")
+    .setHeader(RedisConstants.COMMAND, simple("INCRBY"))
+    .setHeader(RedisConstants.VALUE, simple("${body.count}"))
+    .setHeader(RedisConstants.KEY, simple("${body.candidate}"))
+    .to("spring-redis://{{redis.hostname}}:{{redis.port}}?serializer=#stringSerializer")
     .end()
 
     // Aggregate votes according to original batch.  Split for simple stream processing.
